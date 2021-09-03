7cbdc2dca2af64690c1e8634b8dfb6f17b2bf92b
Chris Dabatos <cdabatos@newrelic.com>
2021-09-02 18:25:09 -0700

    Initial commit

---
+++ src/js/vibe.js
@@ -0,0 +1,12 @@
+const vibes = [
+    "...and you are awesome!",
+    "... have a wonderful day!",
+    "...and you've got this!",
+    "...and so is this puppy!"
+];
+
+//choose a random good vibe
+var vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
+
+// display a good vibe
+document.querySelector(".vibe").append(vibe);
