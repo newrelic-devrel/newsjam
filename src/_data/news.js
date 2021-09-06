f378a814f66a93895ecfaf400e08c9ee8504d427
RealChrisSean <realchrissean@gmail.com>
2021-09-06 07:31:49 -0700

    created more descriptive API variable.

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 module.exports = async function() {
     try {
-      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=6`);
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=6`);
       return response.data;
     } catch (error) {
       console.error(error);
