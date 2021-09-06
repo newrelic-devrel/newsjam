a1489f329f0a75af1dda737cdc93104d7f6f87d2
RealChrisSean <realchrissean@gmail.com>
2021-09-06 07:28:36 -0700

    newsapi.org

---
+++ src/_data/news.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+require('dotenv').config()
+
+module.exports = async function() {
+    try {
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=6`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+
+  };
